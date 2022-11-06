a454ed6f562e673b7b5451ea7135695abe691dff
Brandon Isaac Fonseca <isaac99.bf@gmail.com>
2022-11-05 23:36:32 -0600

    adding new children pages

--- src/app/pages/children-pages/admin-school/admin-school.component.ts
+++ src/app/pages/children-pages/admin-school/admin-school.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'pgch-admin-school',
   templateUrl: './admin-school.component.html',
-  styleUrls: ['./admin-school.component.css']
+  styleUrls: ['./admin-school.component.css'],
 })
 export class AdminSchoolComponent implements OnInit {
+  @Input() buttonName = 'Create button';
+  @Input() infoText =
+    'In this panel you can create some classes to add to the college, to start creating just click below!';
+  @Input() title = 'Elements Title'
+    constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
---
+++ src/app/pages/children-pages/admin-school/classes-panel/classes-panel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'comp-classes-panel',
+  templateUrl: './classes-panel.component.html',
+  styleUrls: ['./classes-panel.component.css']
+})
+export class ClassesPanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
